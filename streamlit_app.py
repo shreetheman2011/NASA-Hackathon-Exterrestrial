e4145bd501873f844fde324c829b0616dd74bc4d
shreetheman2011 <154644066+shreetheman2011@users.noreply.github.com>
2025-09-30 01:29:27 +0000

    change metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("Orbit-Ops")
+st.title("Orbit Ops")
+st.set_page_config(
+        page_title="Orbit Ops", 
+        page_icon="🪐",         
+       
+)
 st.write(
     "We're dedicated to doing stuff."
 )
